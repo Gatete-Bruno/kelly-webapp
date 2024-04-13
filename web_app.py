7b5f0da08b2f96fb3d86b8a1c27e986dd3044d73
GATETE KATO BRUNO <64279605+Gatete-Bruno@users.noreply.github.com>
2024-04-13 23:26:13 +0200

    webapp changes

--- web_app.py
+++ web_app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Set up SQLAlchemy
